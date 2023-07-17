192676f39ea948f58df4d48161a21ef4d1bb0367
dhevsabharish <dhev3518@gmail.com>
2023-07-17 15:58:56 +0530

    Onboarding Form Initial Setup

--- src/components/form/OnboardingForm.tsx
+++ src/components/form/OnboardingForm.tsx
@@ -1,274 +1,161 @@
 import React, { useState } from "react";
 import "./onboardingform.css";
+import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+import Form from "@rjsf/mui";
+
+const schema: RJSFSchema = {
+  title: "Module Onboarding",
+  description: "A Simple Form for developers to simplify Module Onboarding",
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+      title: "Module Name",
+    },
+    protocol: {
+      type: "string",
+      title: "Module Protocol",
+      examples: ["http", "gRPC"],
+    },
+    categories: {
+      type: "array",
+      title: "Categories",
+      items: {
+        type: "object",
+        title: "Category",
+        properties: {
+          name: {
+            type: "string",
+            title: "Category Name",
+          },
+          values: {
+            type: "array",
+            title: "Category Values",
+            items: {
+              $ref: "#/definitions/category",
+            },
+          },
+        },
+        required: ["name"],
+      },
+    },
+    testCaseFields: {
+      type: "array",
+      title: "Custom Testcase Fields",
+      items: {
+        $ref: "#/definitions/testCaseField",
+      },
+    },
+  },
+  required: ["name", "protocol"],
+  definitions: {
+    category: {
+      type: "object",
+      title: "Category",
+      properties: {
+        name: {
+          type: "string",
+          title: "Category Name",
+        },
+        values: {
+          type: "array",
+          title: "Category Values",
+          items: {
+            $ref: "#/definitions/category",
+          },
+        },
+      },
+      required: ["name"],
+    },
+    testCaseField: {
+      type: "object",
+      title: "Test Case Field",
+      properties: {
+        name: {
+          type: "string",
+          title: "Test Case Field Name",
+        },
+        type: {
+          type: "string",
+          title: "Test Case Field Type",
+          examples: ["string", "number", "boolean", "date", "array", "object"],
+        },
+        default: {
+          type: "string",
+          title: "Default Test Case Field Type",
+          examples: ["string", "number", "boolean", "date", "array", "object"],
+        },
+        description: {
+          type: "string",
+          title: "Test Case Field Description",
+        },
+        required: {
+          type: "boolean",
+          title: "Test Case Field Required or Not",
+        },
+      },
+      required: ["name", "type", "required"],
+    },
+  },
+};
+
+const uiSchema: UiSchema = {
+  name: {
+    "ui:autofocus": true,
+    "ui:emptyValue": "",
+  },
+  protocol: {
+    "ui:emptyValue": "",
+  },
+  categories: {
+    items: {
+      name: {
+        "ui:autofocus": true,
+        "ui:emptyValue": "",
+      },
+      values: {
+        items: {
+          name: {
+            "ui:autofocus": true,
+            "ui:emptyValue": "",
+          },
+        },
+      },
+    },
+  },
+  testCaseFields: {
+    items: {
+      name: {
+        "ui:autofocus": true,
+        "ui:emptyValue": "",
+      },
+      type: {
+        "ui:emptyValue": "",
+      },
+      default: {
+        "ui:emptyValue": "",
+      },
+      description: {
+        "ui:widget": "textarea",
+      },
+    },
+  },
+};
 
 const OnboardingForm = () => {
-  const [moduleName, setModuleName] = useState("");
-  const [moduleProtocol, setModuleProtocol] = useState("http");
-  const [testCases, setTestCases] = useState([
-    {
-      name: "",
-      description: "",
-      type: "",
-      required: false,
-      defaultValue: "",
-    },
-  ]);
-  const [categories, setCategories] = useState([
-    {
-      name: "",
-      values: [{ name: "", values: [] }],
-    },
-  ]);
-
-  const handleAddTestCase = () => {
-    setTestCases([
-      ...testCases,
-      {
-        name: "",
-        description: "",
-        type: "",
-        required: false,
-        defaultValue: "",
-      },
-    ]);
-  };
-
-  const handleAddCategory = () => {
-    setCategories([
-      ...categories,
-      {
-        name: "",
-        values: [{ name: "", values: [] }],
-      },
-    ]);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // print all form data to console
-    console.log(moduleName);
-    console.log(moduleProtocol);
-    console.log(testCases);
-    console.log(categories);
-  };
-
-  const handleReset = () => {
-    setModuleName("");
-    setModuleProtocol("http");
-    setTestCases([
-      {
-        name: "",
-        description: "",
-        type: "",
-        required: false,
-        defaultValue: "",
-      },
-    ]);
-    setCategories([
-      {
-        name: "",
-        values: [{ name: "", values: [] }],
-      },
-    ]);
+  const onSubmit = (data) => {
+    console.log("Data submitted: ", data.formData);
   };
 
   return (
-    <div className="onboardingform">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Module Name:
-          <input
-            type="text"
-            value={moduleName}
-            onChange={(event) => setModuleName(event.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Module Protocol:
-          <select
-            value={moduleProtocol}
-            onChange={(event) => setModuleProtocol(event.target.value)}
-          >
-            <option value="http">http</option>
-            <option value="gRPC">gRPC</option>
-            {/* Add more options here */}
-          </select>
-        </label>
-        <br />
-        <label>
-          Test Cases:
-          {testCases.map((testCase, index) => (
-            <div key={index}>
-              <label>
-                Test Case Name:
-                <input
-                  type="text"
-                  value={testCase.name}
-                  onChange={(event) => {
-                    const newTestCases = [...testCases];
-                    newTestCases[index].name = event.target.value;
-                    setTestCases(newTestCases);
-                  }}
-                />
-              </label>
-              <br />
-              <label>
-                Test Case Description:
-                <input
-                  type="text"
-                  value={testCase.description}
-                  onChange={(event) => {
-                    const newTestCases = [...testCases];
-                    newTestCases[index].description = event.target.value;
-                    setTestCases(newTestCases);
-                  }}
-                />
-              </label>
-              <br />
-              <label>
-                Test Case Type:
-                <input
-                  type="text"
-                  value={testCase.type}
-                  onChange={(event) => {
-                    const newTestCases = [...testCases];
-                    newTestCases[index].type = event.target.value;
-                    setTestCases(newTestCases);
-                  }}
-                />
-              </label>
-              <br />
-              <label>
-                Required:
-                <input
-                  type="checkbox"
-                  checked={testCase.required}
-                  onChange={(event) => {
-                    const newTestCases = [...testCases];
-                    newTestCases[index].required = event.target.checked;
-                    setTestCases(newTestCases);
-                  }}
-                />
-              </label>
-              <br />
-              <label>
-                Default Value:
-                <input
-                  type="text"
-                  value={testCase.defaultValue}
-                  onChange={(event) => {
-                    const newTestCases = [...testCases];
-                    newTestCases[index].defaultValue = event.target.value;
-                    setTestCases(newTestCases);
-                  }}
-                />
-              </label>
-              <br />
-            </div>
-          ))}
-          <button type="button" onClick={handleAddTestCase}>
-            Add Test Case
-          </button>
-        </label>
-        <br />
-        <label>
-          Categories:
-          {categories.map((category, index) => (
-            <div key={index}>
-              <label>
-                Category Name:
-                <input
-                  type="text"
-                  value={category.name}
-                  onChange={(event) => {
-                    const newCategories = [...categories];
-                    newCategories[index].name = event.target.value;
-                    setCategories(newCategories);
-                  }}
-                />
-              </label>
-              <br />
-              <label>
-                Values:
-                {category.values.map((value, valueIndex) => (
-                  <div key={valueIndex}>
-                    <label>
-                      Value Name:
-                      <input
-                        type="text"
-                        value={value.name}
-                        onChange={(event) => {
-                          const newCategories = [...categories];
-                          newCategories[index].values[valueIndex].name =
-                            event.target.value;
-                          setCategories(newCategories);
-                        }}
-                      />
-                    </label>
-                    <br />
-                    <label>
-                      Sub-Values:
-                      {value.values.map((subValue, subValueIndex) => (
-                        <div key={subValueIndex}>
-                          <label>
-                            Sub-Value Name:
-                            <input
-                              type="text"
-                              value={subValue.name}
-                              onChange={(event) => {
-                                const newCategories = [...categories];
-                                newCategories[index].values[valueIndex].values[
-                                  subValueIndex
-                                ].name = event.target.value;
-                                setCategories(newCategories);
-                              }}
-                            />
-                          </label>
-                          <br />
-                        </div>
-                      ))}
-                      <button
-                        type="button"
-                        onClick={() => {
-                          const newCategories = [...categories];
-                          newCategories[index].values[valueIndex].values.push({
-                            name: "",
-                            values: [],
-                          });
-                          setCategories(newCategories);
-                        }}
-                      >
-                        Add Sub-Value
-                      </button>
-                    </label>
-                    <br />
-                  </div>
-                ))}
-                <button
-                  type="button"
-                  onClick={() => {
-                    const newCategories = [...categories];
-                    newCategories[index].values.push({ name: "", values: [] });
-                    setCategories(newCategories);
-                  }}
-                >
-                  Add Value
-                </button>
-              </label>
-              <br />
-            </div>
-          ))}
-          <button type="button" onClick={handleAddCategory}>
-            Add Category
-          </button>
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-        <button type="button" onClick={handleReset}>
-          Reset
-        </button>
-      </form>
+    <div className="onboarding-form">
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onSubmit={onSubmit}
+        validator={validator}
+      />
     </div>
   );
 };
